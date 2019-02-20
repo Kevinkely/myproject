ef4747fdc048ad624cdd1cd9c2dd6600117ccb97
Kevinkely <kevo.makosh@gmail.com>
2019-02-20 21:03:49 +0300

    initial commit

---
+++ listings/choices.py
@@ -0,0 +1,41 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'Kes.10,000',
+  '200000':'Kes.20,000',
+  '300000':'Kes.30,000',
+  '400000':'Kes.40,000',
+  '500000':'Kes.50,000',
+  '600000':'Kes.60,000',
+  '700000':'Kes.70,000',
+  '800000':'Kes.80,000',
+  '900000':'Kes.90,000',
+  '1000000':'Kes.100,000',
+  '1000000':'Kes.100k+',
+}
+
+areacode_choices = {
+        'MG': 'Muthaiga',
+        'GG': 'Gigiri',
+        'LAVI': 'Lavington',
+        'KILE': 'Kileleshwa',
+        'RG': 'Ridgeways',
+        'RND': 'Runda',
+        'KILI': 'Kilimani',
+        'HURL': 'Hurlingham',
+        'DC': 'Karen',
+        'DE': 'Kitisuru',
+        'FL': 'Riverside',
+
+}
